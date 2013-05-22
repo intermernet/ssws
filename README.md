3999831ce3e0b7efb8f51506ca2f5a8c43016a8a
Mike Hughes <intermernet@gmail.com>
2013-05-22 17:24:14 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+ssws is a Simple Static Web Server written in Go
+
+It has 2 optional flags:
+
+`-port` will set the TCP/IP port to listen on (defaults to port 80)
+
+`-local` controls if the server is only listening on `localhost` (Set true or false, defaults to true)
---
+++ ssws.go
@@ -0,0 +1,29 @@
+package main
+
+import "net/http"
+import "log"
+import "flag"
+
+var port string
+var local bool
+
+func init() {
+	flag.StringVar(&port, "port", "80", "TCP/IP Port to listen on")
+	flag.BoolVar(&local, "local", true, "Listen on localhost only")
+}
+
+func main() {
+
+	flag.Parse()
+
+	adr := "localhost:" + port
+
+	if local != true {
+		adr = ":" + port
+	}
+
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("www/"))))
+	if err := http.ListenAndServe(adr, nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
