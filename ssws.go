a93db9f962c6560c3a69efff8353fb9c1b8d27c2
Mike Hughes <intermernet@gmail.com>
2013-05-23 22:01:47 +1000

    Add -urlpath flag

--- ssws.go
+++ ssws.go
@@ -9,26 +9,31 @@ import (
 )
 
 var port string
-var path string
 var local bool
+var path string
+var urlpath string
 
 func init() {
 	flag.StringVar(&port, "port", "80", "TCP/IP Port to listen on")
-	flag.StringVar(&path, "path", "www", "Path to export")
 	flag.BoolVar(&local, "local", true, "Listen on localhost only")
+	flag.StringVar(&path, "path", "www", "Path to serve files from")
+	flag.StringVar(&urlpath, "urlpath", "/", "URL Path to export")
 }
 
 func main() {
 	flag.Parse()
-	ps := string(os.PathSeparator)
 	adr := "localhost:" + port
-	if !strings.HasSuffix(path, ps) {
-		path = path + ps
-	}
 	if local != true {
 		adr = ":" + port
 	}
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(path))))
+	ps := string(os.PathSeparator)
+	if !strings.HasSuffix(path, ps) {
+		path = path + ps
+	}
+	if !strings.HasPrefix(urlpath, "/") {
+		urlpath = "/" + urlpath
+	}
+	http.Handle(urlpath, http.StripPrefix(urlpath, http.FileServer(http.Dir(path))))
 	if err := http.ListenAndServe(adr, nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
