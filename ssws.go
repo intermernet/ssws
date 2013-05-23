3b9bd8dbd2476af5a4577351d3d179c5fb610650
Mike Hughes <intermernet@gmail.com>
2013-05-23 15:35:15 +1000

    Add export path flag

--- ssws.go
+++ ssws.go
@@ -1,28 +1,34 @@
 package main
 
-import "net/http"
-import "log"
-import "flag"
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
 
 var port string
+var path string
 var local bool
 
 func init() {
 	flag.StringVar(&port, "port", "80", "TCP/IP Port to listen on")
+	flag.StringVar(&path, "path", "www", "Path to export")
 	flag.BoolVar(&local, "local", true, "Listen on localhost only")
 }
 
 func main() {
-
 	flag.Parse()
-
+	ps := string(os.PathSeparator)
 	adr := "localhost:" + port
-
+	if !strings.HasSuffix(path, ps) {
+		path = path + ps
+	}
 	if local != true {
 		adr = ":" + port
 	}
-
-	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("www/"))))
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(path))))
 	if err := http.ListenAndServe(adr, nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
